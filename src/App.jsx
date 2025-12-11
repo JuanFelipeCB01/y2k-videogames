d6a8993134a8c25177f5e85dcfe223f4f1b231f9
Neo <jfelipecbermudez@gmail.com>
2025-12-11 08:50:35 +0100

    firts request

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
+  const API_KEY = "3192385a-b25d-4fba-befc-876b198b7d63";
+  const BASE_URL = "https://retro.gg/api/"
+
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(Boolean);
+  const [error, setError] = useState();
+
+  const getGames = async () => {
+    try {
+      const  data  = await axios.get(`${BASE_URL}search/year/1993?key${API_KEY}`);
+      setGames(data.results);
+    } catch (error) {
+      console.error("Getting games failed");
+      setError(error);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    
+    console.log("Loading:", loading)
+    getGames();
+
+  }, [])
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Y2K GAMES</h1>
+      {loading  && <div>Loading games...</div>}
+      {error ?? <div>Error: ${error}</div>}
+      {if (!games.length) {
+        games.map((game, index) =>(
+          <div>
+            <p>game.name</p>
+        ))
+        
+      }}
+
+    </div>
   )
 }
 
-export default App
